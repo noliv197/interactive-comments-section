13d94d2d88e21c092a7d2fa53274fa9d7570f7c9
natalia <>
2024-10-02 00:38:34 -0700

    update backend url

--- app/controller/service.js
+++ app/controller/service.js
@@ -1,6 +1,6 @@
-const BACK_URL_DEV = "http://localhost/comment-app"
+const BACK_URL_DEV = "http://localhost:8000"
 const BACK_URL_PRD = "http://commentsection-env-1.eba-w7bqwdmq.us-east-1.elasticbeanstalk.com/comment-app"
-const backendUrl = BACK_URL_PRD + "/index.php"
+const backendUrl = BACK_URL_DEV + "/index.php"
 
 export async function getComments(){
   return await fetch(`${backendUrl}/comments`)
